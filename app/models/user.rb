06b5514e5ca29b7e56d934b1b1e57b72c27721a5
theghall <guategary@gmail.com>
2017-07-11 02:13:39 +0000

    Finish 9.1.4

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_reader :remember_token
+  
   before_save { email.downcase! }
   validates :name, presence: true, length: {maximum: 50}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -9,9 +11,34 @@ class User < ApplicationRecord
                     
   has_secure_password
   
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine::cost
-    BCrypt::Password.create(string, cost: cost)
+  def remember 
+    set_token()
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
+  
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+  
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+  
+  private
+    attr_writer :remember_token
+    
+    def User.digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine::cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+  
+    def User.new_token
+      SecureRandom.urlsafe_base64
+    end
+  
+    def set_token
+      self.remember_token = User.new_token
+    end
 end
