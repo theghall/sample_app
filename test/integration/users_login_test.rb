e72ce704e094cdd94647608f6d155ba9d6fad7a8
theghall <guategary@gmail.com>
2017-07-12 14:11:41 +0000

    Finish chap 9, Add Remember me functionality

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -49,4 +49,15 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_template 'users/show'
     assert is_logged_in?
   end
+  
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_equal cookies['remember_token'], assigns(:user).remember_token
+  end
+  
+  test "login without remembering" do
+    log_in_as(@user, remember_me: '1')
+    log_in_as(@user, remember_me: '0')
+    assert_empty cookies['remember_token']
+  end
 end
