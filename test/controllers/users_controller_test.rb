9ca960d1a08325080fdb10da2553bce411970e49
theghall <guategary@gmail.com>
2017-07-16 20:06:03 +0000

    Finish chapter 10

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,6 +4,8 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   
   def setup
     @base_title = "Ruby on Rails Tutorial Sample App"
+    @user = users(:michael)
+    @other_user = users(:archer)
   end
 
   test "should get new" do
@@ -12,4 +14,28 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "Sign Up | #{@base_title}"
   end
 
+  test "should redirect update when not logged in" do
+    patch user_path(@user), params: { user: { name: @user.name,
+                                              email: @user.email } }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+  
+  test "should redirect destroy when logged in as non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to root_url
+  end
+
+  test "should not allow the admin attribute to be edited via web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+                                    user: { password: "",
+                                            password_confirmation: "",
+                                            admin: true } }
+    assert_not @other_user.reload.admin?
+  end
 end
