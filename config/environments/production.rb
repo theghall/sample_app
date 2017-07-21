dd3a92c55f6c4544cd7f54ecadb4def1f1e82ea6
theghall <guategary@gmail.com>
2017-07-21 05:28:12 +0000

    Fix production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'shielded-hamlet-66447.herokuapp.com'
-  config.action.mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
