a42d080541b4346959ebbe618afccd15f6739722
theghall <guategary@gmail.com>
2017-07-26 01:53:18 +0000

    Finish 13.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
   attr_reader :remember_token
   attr_accessor :activation_token, :reset_token
   before_save :downcase_email
