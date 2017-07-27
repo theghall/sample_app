a88ea07183eda7eb076c25204c186bb04ca2a940
theghall <guategary@gmail.com>
2017-07-27 02:52:07 +0000

    Finish 13.2.2

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,9 @@ User.create!(name: "Example User",
                 activated: true,
                 activated_at: Time.zone.now)
 end
+
+user = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  user.each { |user| user.microposts.create!(content: content) }
+end
